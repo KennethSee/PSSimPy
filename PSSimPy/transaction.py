0352b9cf73e2c180b2ca2de9d7e52314c8bf1da9
Kenneth See <kennethseede@gmail.com>
2024-02-19 01:05:23 +0800

    add priority property to transaction

--- PSSimPy/transaction.py
+++ PSSimPy/transaction.py
@@ -3,10 +3,11 @@ from PSSimPy.utils.constants import TRANSACTION_STATUS_CODES
 
 class Transaction:
 
-    def __init__(self, sender_account: Account, receipient_account: Account, amount: float, **kwargs):
+    def __init__(self, sender_account: Account, receipient_account: Account, amount: float, priority: int=1, **kwargs):
         self.sender_account = sender_account
         self.receipient_account = receipient_account
         self.amount = amount
+        self.priority = priority
         self.status_code = TRANSACTION_STATUS_CODES['Open']
         
         # Use the kwargs to store additional user-defined attributes
