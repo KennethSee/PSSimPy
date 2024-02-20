e87b6fe8a79e6ed3387f59ea79f1e72f1c32985f
Kenneth See <kennethseede@gmail.com>
2024-02-20 20:28:28 +0800

    Provide ability to track transactions created

--- PSSimPy/transaction.py
+++ PSSimPy/transaction.py
@@ -3,6 +3,16 @@ from PSSimPy.utils.constants import TRANSACTION_STATUS_CODES
 
 class Transaction:
 
+    # Class attribute to hold references to all instances
+    _instances = set()
+
+    def __new__(cls, *args, **kwargs):
+        # Create a new instance
+        instance = super().__new__(cls)
+        # Add the new instance to the set of instances
+        cls._instances.add(instance)
+        return instance
+
     def __init__(self, sender_account: Account, receipient_account: Account, amount: float, priority: int=1, **kwargs):
         self.sender_account = sender_account
         self.receipient_account = receipient_account
@@ -19,3 +29,13 @@ class Transaction:
             self.status_code = TRANSACTION_STATUS_CODES[status]
         except KeyError:
             print(f"Invalid status: '{status}'. Please provide a valid transaction status.")
+
+    @classmethod
+    def get_instances(cls):
+        """Provides the set of all created transactions"""
+        return cls._instances
+
+    @classmethod
+    def clear_instances(cls):
+        """Clears tracked transactions created. Should be used before running a new simulation."""
+        cls._instances.clear()
