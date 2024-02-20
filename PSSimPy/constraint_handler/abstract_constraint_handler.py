4ea8a4ba59f2a17497994d180bcefd720fa88276
Kenneth See <kennethseede@gmail.com>
2024-02-20 18:29:41 +0800

    fix simple constraint handler

--- PSSimPy/constraint_handler/abstract_constraint_handler.py
+++ PSSimPy/constraint_handler/abstract_constraint_handler.py
@@ -4,14 +4,13 @@ from PSSimPy import Transaction
 class AbstractConstraintHandler(ABC):
 
     def __init__(self):
-        # for key, value in kwargs.items():
-        #     setattr(self, key, value)
-        self.processed_transactions = {'pass': [], 'fail': []}
+        self.passed_transactions = []
     
     @abstractmethod
-    def process_transaction(self, transaction: Transaction) -> dict:
-        """
-        Should return a dictionary that consists of 'pass' and 'fail' as keys, 
-        with the corresponding passed and failed transactions as values in the form of a list respectively.
-        """
+    def process_transaction(self, transaction: Transaction) -> list:
+        """Should return a list of passed transactions to be sent for further processing."""
         pass
+
+    def get_passed_transactions(self):
+        """Returns the list of passed transactions."""
+        return self.passed_transactions
--- PSSimPy/constraint_handler/simple_constraint_handler.py
+++ PSSimPy/constraint_handler/simple_constraint_handler.py
@@ -1,4 +1,4 @@
-from PSSimPy.constraint_handler import AbstractSettlementMechanism
+from PSSimPy.constraint_handler import AbstractConstraintHandler
 from PSSimPy import Transaction
 
 class SimpleConstraintHandler(AbstractConstraintHandler):
@@ -8,29 +8,31 @@ class SimpleConstraintHandler(AbstractConstraintHandler):
         super().__init__()
         self.max_txn_size = max_txn_size
 
-    def process_transaction(self, transaction: Transaction) -> dict:
-        sender = transaction.sender_account
-        receipient = transaction.receipient_account
-        # check if transaction size exceeds max size
+    # implement abstract function
+    def process_transaction(self, transaction: Transaction):
+        """Process a transaction, splitting it if it exceeds max size and ensuring all resulting transactions pass."""
+        kwargs = (transaction.kwargs if hasattr(transaction, 'kwargs') else {})
+
+        # Check if transaction size exceeds max size
         if transaction.amount > self.max_txn_size:
-            # split transactions
-            tx1 = Transaction(
-                sender_account = transaction.sender_account,
-                receipient_account = transaction.receipient_account,
-                amount = self.max_txn_size,
-                priority = transaction.priority,
-                transaction.kwargs
-                )
-            tx2 = Transaction(
-                sender_account = transaction.sender_account,
-                receipient_account = transaction.receipient_account,
-                amount = transaction.amount - self.max_txn_size,
-                priority = transaction.priority,
-                transaction.kwargs
-                )
-            # to-do: reprocess split transactions
-
+            # Split the transaction
+            txn1 = Transaction(
+                sender_account=transaction.sender_account,
+                receipient_account=transaction.receipient_account,
+                amount=self.max_txn_size,
+                priority=transaction.priority,
+                **kwargs
+            )
+            txn2 = Transaction(
+                sender_account=transaction.sender_account,
+                receipient_account=transaction.receipient_account,
+                amount=transaction.amount - self.max_txn_size,
+                priority=transaction.priority,
+                **kwargs
+            )
+            # Recursively process the split transactions
+            self.process_transaction(txn1)
+            self.process_transaction(txn2)
         else:
-            self.processed_transactions['pass'].append(transaction)
-
-        return self.processed_transactions
+            # If transaction does not exceed max size, consider it passed
+            self.passed_transactions.append(transaction)
