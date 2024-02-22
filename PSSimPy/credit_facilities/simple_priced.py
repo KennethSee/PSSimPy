b2777f13a9a46428e6d47d78d9c23926d90a9450
hanzholahs <hanzholahs@gmail.com>
2024-02-22 14:18:36 +0700

    modify implementation that uses function to calc fee

--- PSSimPy/credit_facilities/simple_priced.py
+++ PSSimPy/credit_facilities/simple_priced.py
@@ -1,23 +1,29 @@
+from collections import defaultdict
+
 from PSSimPy.credit_facilities.abstract_credit_facility import AbstractCreditFacility
 from PSSimPy.account import Account
 
 
 class SimplePriced(AbstractCreditFacility):
     
-    def __init__(self, price: float = 0, rate: float = 0) -> None:
+    def __init__(self, base_fee: float = 0, base_rate: float = 0) -> None:
         super().__init__()
-        self.price = price
-        self.rate = rate
+        self.base_fee = base_fee
+        self.base_rate = base_rate
+        
+    def calculate_fee(self, credit_amount: float) -> float:
+        return credit_amount * self.base_rate + self.base_fee
     
     def lend_credit(self, account: Account, amount: float) -> float:
+        self.used_credit[account.id].append(amount)
         account.balance += amount
-        self.total_credit[account.id] += amount
-        self.total_fee[account.id] += (amount * self.rate) + self.price
     
     def collect_repayment(self, account: Account) -> None:
-        if self.get_total_credit_and_fee(account) >= account.balance:
-            return
-        account.balance -= self.total_credit[account.id]
-        account.balance -= self.total_fee[account.id]
-        self.total_credit[account.id] = 0
-        self.total_fee[account.id] = 0
+        for i, credit_amount in enumerate(self.used_credit[account.id]):
+            fee = self.calculate_fee(credit_amount)
+            
+            if credit_amount + fee <= account.balance:
+                account.balance -= (credit_amount + fee)
+                self.used_credit[account.id][i] = 0
+        
+        self.used_credit[account.id] = [x for x in self.used_credit[account.id] if x != 0]
