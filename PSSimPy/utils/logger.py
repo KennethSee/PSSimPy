9aa94cc08d0d20a505f3003a8f27973a0f603bbb
Kenneth See <kennethseede@gmail.com>
2024-02-27 02:29:48 +0800

    create logger class

---
+++ PSSimPy/utils/logger.py
@@ -0,0 +1,25 @@
+import os
+import csv
+
+class Logger:
+
+    def __init__(self, file_path: str, headers: tuple):
+        # append ".csv" if not included in file name
+        if not file_path.endswith('.csv'):
+            file_path = file_path + '.csv'
+        self.file_path = file_path
+        self.headers = headers
+
+    def write(self, data: list[tuple]):
+        if not os.path.exists(self.file_path):
+            data.insert(0, self.headers)
+            mode = 'w'
+        else:
+            mode = 'a'
+        with open(self.file_path, mode, newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(data)
+
+logger = Logger('test', ('Hello', 'Wolrd'))
+data = [('h1', 'w1'), ('h2', 'w2')]
+logger.write(data)
