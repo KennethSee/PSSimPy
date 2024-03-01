897a892b201dad3b98fcffd533576f01a9cc1e53
Kenneth See <kennethseede@gmail.com>
2024-03-01 22:30:22 +0800

    implement Bank

--- PSSimPy/account.py
+++ PSSimPy/account.py
@@ -2,10 +2,14 @@ from PSSimPy.bank import Bank
 
 class Account:
 
-    def __init__(self, id: str, owner: Bank, balance: float):
+    def __init__(self, id: str, owner: Bank, balance: float, **kwargs):
         self.id = id
         self.owner = owner
         self.balance = balance
+
+        # Use the kwargs to store additional user-defined attributes
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def transfer_to(self, receipient: 'Account', amount: float) -> None:
         self.balance -= amount
--- PSSimPy/bank.py
+++ PSSimPy/bank.py
@@ -1,4 +1,17 @@
+from PSSimPy.queues.abstract_queue import AbstractQueue
+from PSSimPy.transaction import Transaction
+
+
 class Bank:
 
-    def __init__(self):
+    def __init__(self, name: str, strategy_type: str='Normal', **kwargs):
+        self.name = name
+        self.strategy_type = strategy_type
+
+        # Use the kwargs to store additional user-defined attributes
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    # only relevant for agent-based modeling
+    def strategy(self, queue: AbstractQueue, txns_to_settle: list[Transaction]) -> set[Transaction]:
         pass
