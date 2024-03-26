b80a2b72c549c85a63f3a214738b7b44545d7451
hanzholahs <hanzholahs@gmail.com>
2024-03-26 21:57:16 +0700

    make import all objects in logger

--- PSSimPy/utils/__init__.py
+++ PSSimPy/utils/__init__.py
@@ -3,4 +3,4 @@ from PSSimPy.utils.constants import *
 from PSSimPy.utils.transaction_utils import *
 from PSSimPy.utils.time_utils import *
 from PSSimPy.utils.file_utils import *
-from PSSimPy.utils.logger import Logger
+from PSSimPy.utils.logger import *
