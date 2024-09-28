7c7f8f9068c536e2a3192cbcf29bd8d1c682472d
Kenneth See <kennethseede@gmail.com>
2024-09-28 23:46:31 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='PSSimPy',
-    version='0.1.1',
+    version='0.1.2',
     author='Kenneth See, Hanzholah Shobri',
     author_email='see.k@u.nus.edu, hanzhshobri@gmail.com',
     packages=find_packages(),
